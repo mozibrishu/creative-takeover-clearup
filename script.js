09b80a59a8b8b955e12f4fd218651fc3b15de28e
Mozibur Rahman <mozib.risu@gmail.com>
2025-07-21 23:55:41 +0600

    desktop mode ready

--- script.js
+++ script.js
@@ -10,15 +10,30 @@ document.querySelector('.wbl_closeBtn_takeover').addEventListener('click', (e) =
   removedAll()
 })
 setTimeout(() => {
-  removedAll()
+  // removedAll()
 }, 17000);
 
-var isMobile = window.innerWidth < 600;
 
 function beginAnimation() {
+  var isMobile = window.innerWidth < 600;
   gsap.timeline()
     .set('.wbl_innerContainer', { display: 'block' })
+    .fromTo('.wbl_can_container', {y:'150%',display:'none'},{y:'0%',display:'block',duration:1,ease:'none'})
+    .fromTo('.wbl_can_wrapper>img',{x:"32%"},{x:'-32%',duration:1,ease:'none',onComplete:wrapperImgAnim},'<')
+    .fromTo('.wbl_kv',{x:'60%',display:'none',opacity:0},{x:'0%',display:'block',opacity:1,duration:.7,ease:'none'},'>.5')
+    .to('.wbl_can_container',{x:'175%',duration:.7},"<")
+    .fromTo('.wbl_logo',{display:'none',opacity:0},{display:'block',opacity:1,duration:.7,ease:'none',onComplete:logoAnimation},'>.5')
+}
+
+setTimeout(() => {
+  beginAnimation();
+}, 1000);
+
+function wrapperImgAnim(){
+  gsap.to('.wbl_can_wrapper>img',{x:"-30%",duration:1,ease:'none',repeat:-1, yoyo:true})
 }
 
 
-beginAnimation();
+function logoAnimation() {
+  gsap.fromTo('.wbl_logo', {rotationZ: 0, opacity: 0, scale: 0.5}, {rotationZ: 360, opacity: 1, scale: 1, duration: 1, ease: "back.out(1.7)", repeat: -1, repeatDelay: 2}, '<.2');
+}
